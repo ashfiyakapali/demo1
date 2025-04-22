7240a02df0a7c845c24c6909cec5251cb1246f6d
ashfiyakapali <ashfiyakapali@gmail.com>
2025-04-22 09:56:27 +0530

    saved

--- demo.java
+++ demo.java
@@ -1,6 +1,7 @@
 public class demo {
         public static void main(String[] args) {
-            System.out.println("MAIN");  // Corrected syntax
+            System.out.println("MAIN"); 
+            System.out.println("MASTER");   // Corrected syntax
         }
     }
 
